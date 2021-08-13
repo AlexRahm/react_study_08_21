105f91071d4d9183d5148a8b56a9507fa301bc30
AlexRahm <venome18@gmail.com>
2021-08-13 23:03:34 +0300

    add_counter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Counter from './components/Countet';
 
 function App() {
-const [likes, setLikes] = useState(5)
-const [value, setValue] = useState('Какой то текст')
-
-function incriment () {
-  setLikes(likes + 1 )
-}
-
-function decriment () {
-  setLikes(likes - 1)
-}
-
-  return (
-    <div className="App">
-      <br/>
-      <h2>{likes}</h2>
-      <h1>{value}</h1>
-      <input type='text' value={value} onChange={event => setValue(event.target.value)}></input>
-      <button onClick = {incriment}>incriment</button>
-      <button onClick = {decriment}>decriment</button>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <br />
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
